c8c0d3ba894c569bbddd8e6a3c8ea85cab38b9ee
nopjar <code.nopjar@gmail.com>
2022-10-28 14:45:14 +0200

    fix: wrong order of parameters in Assertions#assertEquals(long, long)
    The method expects two longs. The first one is the expected value
    and the second long is the actual value. Even though this seems like a
    trivial change, the resulting output if a test fails is significantly
    different.

--- w02h01/test/pgdp/UnitTests.java
+++ w02h01/test/pgdp/UnitTests.java
@@ -8,49 +8,49 @@ public class UnitTests {
     @Test
     public void checkPenguEvolutions()
     {
-        Assertions.assertEquals(PenguWarmup.penguEvolution(128, 2), 4);
-        Assertions.assertEquals(PenguWarmup.penguEvolution(9, 9), 7);
-        Assertions.assertEquals(PenguWarmup.penguEvolution(9, 10), 22);
+        Assertions.assertEquals(4, PenguWarmup.penguEvolution(128, 2));
+        Assertions.assertEquals(7, PenguWarmup.penguEvolution(9, 9));
+        Assertions.assertEquals(22, PenguWarmup.penguEvolution(9, 10));
     }
 
     @Test
     public void checkPenguSum()
     {
-        Assertions.assertEquals(PenguWarmup.penguSum(128), 11);
-        Assertions.assertEquals(PenguWarmup.penguSum(1337), 14);
-        Assertions.assertEquals(PenguWarmup.penguSum(54354), 21);
-        Assertions.assertEquals(PenguWarmup.penguSum(1000), 1);
-        Assertions.assertEquals(PenguWarmup.penguSum(0), 0);
-        Assertions.assertEquals(PenguWarmup.penguSum(9), 9);
-        Assertions.assertEquals(PenguWarmup.penguSum(4004), 8);
-        Assertions.assertEquals(PenguWarmup.penguSum(1234567), 28);
+        Assertions.assertEquals(11, PenguWarmup.penguSum(128));
+        Assertions.assertEquals(14, PenguWarmup.penguSum(1337));
+        Assertions.assertEquals(21, PenguWarmup.penguSum(54354));
+        Assertions.assertEquals(1, PenguWarmup.penguSum(1000));
+        Assertions.assertEquals(0, PenguWarmup.penguSum(0));
+        Assertions.assertEquals(9, PenguWarmup.penguSum(9));
+        Assertions.assertEquals(8, PenguWarmup.penguSum(4004));
+        Assertions.assertEquals(28, PenguWarmup.penguSum(1234567));
     }
 
     @Test
     public void checkPenguPermutation()
     {
-        Assertions.assertEquals(PenguWarmup.penguPermutation(6, 3), 120);
-        Assertions.assertEquals(PenguWarmup.penguPermutation(21, 19), 420);
-        Assertions.assertEquals(PenguWarmup.penguPermutation(10, 7), 720);
-        Assertions.assertEquals(PenguWarmup.penguPermutation(42, 42), 1);
-        Assertions.assertEquals(PenguWarmup.penguPermutation(500000000000001L, 500000000000000L), 500000000000001L);
+        Assertions.assertEquals(120, PenguWarmup.penguPermutation(6, 3));
+        Assertions.assertEquals(420, PenguWarmup.penguPermutation(21, 19));
+        Assertions.assertEquals(720, PenguWarmup.penguPermutation(10, 7));
+        Assertions.assertEquals(1, PenguWarmup.penguPermutation(42, 42));
+        Assertions.assertEquals(500000000000001L, PenguWarmup.penguPermutation(500000000000001L, 500000000000000L));
     }
 
     @Test
     public void checkPenguPowers()
     {
-        Assertions.assertEquals(PenguWarmup.penguPowers(1337, 2), 1787569);
-        Assertions.assertEquals(PenguWarmup.penguPowers(-1337, 2), 1787569);
-        Assertions.assertEquals(PenguWarmup.penguPowers(-8, 3), -512);
-        Assertions.assertEquals(PenguWarmup.penguPowers(3, 4), 81);
-        Assertions.assertEquals(PenguWarmup.penguPowers(3, 0), 1);
-        Assertions.assertEquals(PenguWarmup.penguPowers(9, 1), 9);
-        Assertions.assertEquals(PenguWarmup.penguPowers(-9, 2), 81);
-        Assertions.assertEquals(PenguWarmup.penguPowers(-3, 3), -27);
-        Assertions.assertEquals(PenguWarmup.penguPowers(1, -1),1);
-        Assertions.assertEquals(PenguWarmup.penguPowers(1, -42),1);
-        Assertions.assertEquals(PenguWarmup.penguPowers(0, 1), 0);
-        Assertions.assertEquals(PenguWarmup.penguPowers(0, 0), 1);
+        Assertions.assertEquals(1787569, PenguWarmup.penguPowers(1337, 2));
+        Assertions.assertEquals(1787569, PenguWarmup.penguPowers(-1337, 2));
+        Assertions.assertEquals(-512, PenguWarmup.penguPowers(-8, 3));
+        Assertions.assertEquals(81, PenguWarmup.penguPowers(3, 4));
+        Assertions.assertEquals(1, PenguWarmup.penguPowers(3, 0));
+        Assertions.assertEquals(9, PenguWarmup.penguPowers(9, 1));
+        Assertions.assertEquals(81, PenguWarmup.penguPowers(-9, 2));
+        Assertions.assertEquals(-27, PenguWarmup.penguPowers(-3, 3));
+        Assertions.assertEquals(1, PenguWarmup.penguPowers(1, -1));
+        Assertions.assertEquals(1, PenguWarmup.penguPowers(1, -42));
+        Assertions.assertEquals(0, PenguWarmup.penguPowers(0, 1));
+        Assertions.assertEquals(1, PenguWarmup.penguPowers(0, 0));
     }
 
 }
