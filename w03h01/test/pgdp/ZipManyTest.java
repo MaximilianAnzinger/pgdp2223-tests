ec2e7744d80f6e681bf265cb8f9afecc1ae605bf
Felix Hauptmann <git@felix-hauptmann.de>
2022-11-07 22:18:50 +0100

    rearrange input numbers to generate ascending sequence as output

--- w03h01/test/pgdp/ZipManyTest.java
+++ w03h01/test/pgdp/ZipManyTest.java
@@ -55,10 +55,10 @@ public class ZipManyTest {
                 ),
                 arguments(
                         new int[][]{
-                                {2, 4},
-                                {1, 2, 3}
+                                {1, 3},
+                                {2, 4, 5}
                         },
-                        new int[]{2, 1, 4, 2, 3}
+                        new int[]{1, 2, 3, 4, 5}
                 )
         );
     }
