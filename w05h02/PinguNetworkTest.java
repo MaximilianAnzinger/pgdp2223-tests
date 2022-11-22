827f5ee68ae2f8888ab47010cb0c5a526e327f4e
coderic_ <76266284+cod-eric@users.noreply.github.com>
2022-11-22 12:47:41 +0100

    fixed tests for W05H02

--- w05h02/PinguNetworkTest.java
+++ w05h02/PinguNetworkTest.java
@@ -3,22 +3,21 @@ package pgdp.pingunetwork;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class PinguNetworkTest {
-    @Test
-    @DisplayName("User - Create a user with no profile picture")
-    public void testUser1(){
-        User bob = new User("Bob", "i code stuff");
-        assert(bob.getName().equals("Bob"));
-        assert(bob.getDescription().equals("i code stuff"));
-        assert(bob.getProfilePicture() == null);
-    }
+
+    private Picture pic1 = new Picture("abc", new int[5][4]);
+    private Picture pic2 = new Picture("testest", new int[0][4]);
+    private Picture pic3 = new Picture("", new int[][]{{0, 0, 0}, {0, 1, 1}, {0, -6, 3}});
+    private Picture pic4 = new Picture("3", new int[7][0]);
 
     @Test
     @DisplayName("User - Interact with own post")
-    public void testUser2(){
-        User bob = new User("Bob", "i code stuff");
+    public void testUser2() {
+        User bob = new User("Bob", "i code stuff", pic1);
         bob.post("Rainy day", "2cm of water already");
         Post bobs_post = bob.getPosts()[0];
 
@@ -31,10 +30,10 @@ public class PinguNetworkTest {
 
     @Test
     @DisplayName("User - Multiple interactions from other users")
-    public void testUser3(){
-        User bob = new User("Bob", "i code stuff");
-        User alice = new User("Alice", "lol");
-        User joe = new User("Joe", "jell-o is tasty");
+    public void testUser3() {
+        User bob = new User("Bob", "i code stuff", pic1);
+        User alice = new User("Alice", "lol", pic2);
+        User joe = new User("Joe", "jell-o is tasty", pic2);
 
         bob.post("Rainy day", "2cm of water already");
         Post bobs_post = bob.getPosts()[0];
@@ -51,8 +50,8 @@ public class PinguNetworkTest {
 
     @Test
     @DisplayName("User - Post")
-    public void testUser4(){
-        User bob = new User("Bob", "i code stuff");
+    public void testUser4() {
+        User bob = new User("Bob", "i code stuff", pic4);
         assertEquals(bob.getPosts().length, 0);
 
         bob.post("sunday vibes", "pgdp again :pain:");
@@ -66,8 +65,8 @@ public class PinguNetworkTest {
 
     @Test
     @DisplayName("User - Post two times")
-    public void testUser5(){
-        User bob = new User("Bob", "i code stuff");
+    public void testUser5() {
+        User bob = new User("Bob", "i code stuff", pic2);
         assertEquals(bob.getPosts().length, 0);
 
         bob.post("sunday vibes", "pgdp again :pain:");
@@ -86,8 +85,8 @@ public class PinguNetworkTest {
 
     @Test
     @DisplayName("User - Comment on own post")
-    public void testUser6(){
-        User bob = new User("Bob", "i code stuff");
+    public void testUser6() {
+        User bob = new User("Bob", "i code stuff", pic3);
         assertEquals(bob.getPosts().length, 0);
 
         bob.post("sunday vibes", "pgdp again :pain:");
@@ -107,9 +106,9 @@ public class PinguNetworkTest {
 
     @Test
     @DisplayName("User - Comment on other user's post")
-    public void testUser7(){
-        User bob = new User("Bob", "i code stuff");
-        User alice = new User("Alice", "lol");
+    public void testUser7() {
+        User bob = new User("Bob", "i code stuff", pic4);
+        User alice = new User("Alice", "lol", pic3);
         assertEquals(bob.getPosts().length, 0);
 
         bob.post("sunday vibes", "pgdp again :pain:");
@@ -127,14 +126,14 @@ public class PinguNetworkTest {
 
     @Test
     @DisplayName("Picture - Create")
-    void testPicture1(){
+    void testPicture1() {
         int[][] picData = {
-                {0,0,1},
-                {1,2,7},
-                {44444,83718,819823738},
+                {0, 0, 1},
+                {1, 2, 7},
+                {44444, 83718, 819823738},
                 {45612, 0, -3}
         };
-        Picture pic = new Picture("Toronto",picData);
+        Picture pic = new Picture("Toronto", picData);
 
         assertEquals("Toronto", pic.getLocation());
         assertArrayEquals(pic.getData(), picData);
@@ -145,35 +144,53 @@ public class PinguNetworkTest {
 
     @Test
     @DisplayName("Picture - Use in User")
-    public void testPicture2(){
-        User bob = new User("Bob", "i code stuff", new Picture("Bayern", new int[][]{{0,0,0},{0,1,0},{0,0,0}}));
-        assert(bob.getProfilePicture().getLocation().equals("Bayern"));
-        assertArrayEquals(bob.getProfilePicture().getData(), new int[][]{{0,0,0},{0,1,0},{0,0,0}});
+    public void testPicture2() {
+        User bob = new User("Bob", "i code stuff", new Picture("Bayern", new int[][]{{0, 0, 0}, {0, 1, 0}, {0, 0, 0}}));
+        assert (bob.getProfilePicture().getLocation().equals("Bayern"));
+        assertArrayEquals(bob.getProfilePicture().getData(), new int[][]{{0, 0, 0}, {0, 1, 0}, {0, 0, 0}});
     }
 
     @Test
     @DisplayName("Picture - Use in Group")
-    public void testPicture3(){
-        User bob = new User("Bob", "i code stuff");
-        Group bob_gang = new Group("zulip-chat", "more recycling symbols than on most bottles", bob, new Picture("Bayern", new int[][]{{0,0,0},{0,1,0},{0,0,0}}));
+    public void testPicture3() {
+        User bob = new User("Bob", "i code stuff", pic1);
+        Group bob_gang = new Group("zulip-chat", "more recycling symbols than on most bottles", bob, new Picture("Bayern", new int[][]{{0, 0, 0}, {0, 1, 0}, {0, 0, 0}}));
 
         assertEquals(bob_gang.getPicture().getLocation(), "Bayern");
-        assertArrayEquals(bob_gang.getPicture().getData(), new int[][]{{0,0,0},{0,1,0},{0,0,0}});
+        assertArrayEquals(bob_gang.getPicture().getData(), new int[][]{{0, 0, 0}, {0, 1, 0}, {0, 0, 0}});
     }
 
     @Test
     @DisplayName("Picture - Set Thumbnails")
-    public void testPicture4(){
-        Picture pic = new Picture("The Länd", new int[][]{{0,0,0},{0,1,0},{0,0,0}});
+    public void testPicture4() {
+        Picture pic = new Picture("The Länd", new int[][]{{0, 0, 0}, {0, 1, 0}, {0, 0, 0}});
         assertArrayEquals(pic.getThumbnails(), new Picture[0]);
 
-        Picture[] thumbnails = new Picture[] {
-                new Picture("münchner freiheit", new int[][]{{0,0,0},{0,4,5},{1,0,0}}),
-                new Picture("entenhausen", new int[][]{{0,0,0},{0,1,0},{0,0,0}})
+        Picture[] thumbnails = new Picture[]{
+                new Picture("münchner freiheit", new int[][]{{0, 0, 0}, {0, 4, 5}, {1, 0, 0}}),
+                new Picture("entenhausen", new int[][]{{0, 0, 0}, {0, 1, 0}, {0, 0, 0}})
         };
         pic.setThumbnails(thumbnails);
 
         assertArrayEquals(pic.getThumbnails(), thumbnails);
+    }
+
+    @Test
+    @DisplayName("Picture - width is 0")
+    public void testPicture5() {
+        Picture pic = new Picture("mnb", new int[5][0]);
+        assertEquals(pic.getWidth(), 0);
+        assertEquals(pic.getHeight(), 5);
+        assertTrue(Arrays.deepEquals(new int[5][0], pic.getData()));
+    }
+
+    @Test
+    @DisplayName("Picture - height is 0")
+    public void testPicture6() {
+        Picture pic = new Picture("mnb", new int[0][7]);
+        assertEquals(pic.getWidth(), 0);
+        assertEquals(pic.getHeight(), 0);
+        assertTrue(Arrays.deepEquals(new int[0][7], pic.getData()));
     }
 
 
@@ -181,8 +198,8 @@ public class PinguNetworkTest {
 
     @Test
     @DisplayName("InteractionTest")
-    void testInteraction1(){
-        User bob = new User("Bob", "i code stuff");
+    void testInteraction1() {
+        User bob = new User("Bob", "i code stuff", pic3);
         Interaction interaction = new Interaction(bob, 4);
 
         assertEquals(interaction.getUser(), bob);
@@ -194,10 +211,10 @@ public class PinguNetworkTest {
 
     @Test
     @DisplayName("Group - remove user (not owner)")
-    void testGroup1(){
-        User bob = new User("Bob", "i code stuff");
-        Group bob_gang = new Group("zulip-chat", "more recycling symbols than on most bottles", bob, new Picture("Bayern", new int[][]{{0,0,0},{0,1,0},{0,0,0}}));
-        User alice = new User("Alice", "lol");
+    void testGroup1() {
+        User bob = new User("Bob", "i code stuff", pic1);
+        Group bob_gang = new Group("zulip-chat", "more recycling symbols than on most bottles", bob, new Picture("Bayern", new int[][]{{0, 0, 0}, {0, 1, 0}, {0, 0, 0}}));
+        User alice = new User("Alice", "lol", pic3);
 
         bob_gang.addUser(alice);
         assertArrayEquals(bob_gang.getMembers(), new User[]{bob, alice});
@@ -210,10 +227,10 @@ public class PinguNetworkTest {
 
     @Test
     @DisplayName("Group - remove owner, members not empty afterwards")
-    void testGroup2(){
-        User bob = new User("Bob", "i code stuff");
-        Group bob_gang = new Group("zulip-chat", "more recycling symbols than on most bottles", bob, new Picture("Bayern", new int[][]{{0,0,0},{0,1,0},{0,0,0}}));
-        User alice = new User("Alice", "lol");
+    void testGroup2() {
+        User bob = new User("Bob", "i code stuff", pic2);
+        Group bob_gang = new Group("zulip-chat", "more recycling symbols than on most bottles", bob, new Picture("Bayern", new int[][]{{0, 0, 0}, {0, 1, 0}, {0, 0, 0}}));
+        User alice = new User("Alice", "lol", pic4);
 
         bob_gang.addUser(alice);
         assertArrayEquals(bob_gang.getMembers(), new User[]{bob, alice});
@@ -226,9 +243,9 @@ public class PinguNetworkTest {
 
     @Test
     @DisplayName("Group - remove owner, members empty afterwards")
-    void testGroup3(){
-        User bob = new User("Bob", "i code stuff");
-        Group bob_gang = new Group("zulip-chat", "more recycling symbols than on most bottles", bob, new Picture("Bayern", new int[][]{{0,0,0},{0,1,0},{0,0,0}}));
+    void testGroup3() {
+        User bob = new User("Bob", "i code stuff", pic1);
+        Group bob_gang = new Group("zulip-chat", "more recycling symbols than on most bottles", bob, new Picture("Bayern", new int[][]{{0, 0, 0}, {0, 1, 0}, {0, 0, 0}}));
 
         bob_gang.removeUser(bob);
         assertArrayEquals(bob_gang.getMembers(), new User[]{});
@@ -237,11 +254,11 @@ public class PinguNetworkTest {
 
     @Test
     @DisplayName("Group - remove nonexistent user")
-    void testGroup4(){
-        User bob = new User("Bob", "i code stuff");
-        Group bob_gang = new Group("zulip-chat", "more recycling symbols than on most bottles", bob, new Picture("Bayern", new int[][]{{0,0,0},{0,1,0},{0,0,0}}));
-        User alice = new User("Alice", "lol");
-        User joe = new User("Joe", "jell-o is tasty");
+    void testGroup4() {
+        User bob = new User("Bob", "i code stuff", pic4);
+        Group bob_gang = new Group("zulip-chat", "more recycling symbols than on most bottles", bob, new Picture("Bayern", new int[][]{{0, 0, 0}, {0, 1, 0}, {0, 0, 0}}));
+        User alice = new User("Alice", "lol", pic2);
+        User joe = new User("Joe", "jell-o is tasty", pic4);
 
         bob_gang.addUser(alice);
 
@@ -252,9 +269,9 @@ public class PinguNetworkTest {
 
     @Test
     @DisplayName("Simulated interactions - 1")
-    void simulateInteraction(){
-        User alice = new User("Alice", "quirky girl", new Picture("Toronto", new int[][]{{1,1,1}, {1,2,3}, {1,1,1}}));
-        User bob = new User("Bob", "nerdy stuff is my hobby", new Picture("Land des Bieres", new int[][]{{4,5,6}, {0,0,4}, {7,8,9}}));
+    void simulateInteraction() {
+        User alice = new User("Alice", "quirky girl", new Picture("Toronto", new int[][]{{1, 1, 1}, {1, 2, 3}, {1, 1, 1}}));
+        User bob = new User("Bob", "nerdy stuff is my hobby", new Picture("Land des Bieres", new int[][]{{4, 5, 6}, {0, 0, 4}, {7, 8, 9}}));
 
         assertEquals(alice.getPosts().length, 0);
 
@@ -274,3 +291,4 @@ public class PinguNetworkTest {
         assertEquals(bob.getPosts()[0], alice.getPosts()[0].getComments()[1]);
     }
 }
+
