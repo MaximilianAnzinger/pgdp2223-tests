3a94884cbdb776192669e8299345902e62ecab78
AleWu <77410271+TheAleWu@users.noreply.github.com>
2022-11-28 00:35:49 +0100

    [W06H01] Implemented pr review suggestion

--- w06h01/test/pgdp/KinguinSortTest.java
+++ w06h01/test/pgdp/KinguinSortTest.java
@@ -11,6 +11,70 @@ import pgdp.datastructures.lists.RecIntList;
 
 public class KinguinSortTest extends TestBase {
 
+    // Edge Case 1
+    @ParameterizedTest(name = "{1} | Increasing: {2}")
+    @MethodSource
+    @DisplayName("kinguinSort works for single element lists")
+    public void testKinguinSortSingleElementList(RecIntList expected, RecIntList r, boolean increasing) {
+        r.kinguinSort(increasing);
+        assertListEquals(expected, r);
+    }
+
+    static Stream<Arguments> testKinguinSortSingleElementList() {
+        return Stream.of(
+            arguments(list(0), list(0), true),
+            arguments(list(0), list(0), false)
+        );
+    }
+
+    // Edge Case 2
+    @ParameterizedTest(name = "{1} | Increasing: {2}")
+    @MethodSource
+    @DisplayName("kinguinSort works for double element lists")
+    public void testKinguinSortDoubleElementList(RecIntList expected, RecIntList r, boolean increasing) {
+        r.kinguinSort(increasing);
+        assertListEquals(expected, r);
+    }
+
+    static Stream<Arguments> testKinguinSortDoubleElementList() {
+        return Stream.of(
+            arguments(list(0, 1), list(0, 1), true),
+            arguments(list(0), list(0, 1), false)
+        );
+    }
+
+    // Edge Case 3
+    @ParameterizedTest(name = "{1} | Increasing: {2}")
+    @MethodSource
+    @DisplayName("kinguinSort works for empty lists")
+    public void testKinguinSortEmptyList(RecIntList expected, RecIntList r, boolean increasing) {
+        r.kinguinSort(increasing);
+        assertListEquals(expected, r);
+    }
+
+    static Stream<Arguments> testKinguinSortEmptyList() {
+        return Stream.of(
+            arguments(list(), list(), true),
+            arguments(list(), list(), false)
+        );
+    }
+
+    // Edge Case 4
+    @ParameterizedTest(name = "{1} | Increasing: {2}")
+    @MethodSource
+    @DisplayName("kinguinSort works for lists with integer border elements")
+    public void testKinguinSortIntegerBorderElements(RecIntList expected, RecIntList r, boolean increasing) {
+        r.kinguinSort(increasing);
+        assertListEquals(expected, r);
+    }
+
+    static Stream<Arguments> testKinguinSortIntegerBorderElements() {
+        return Stream.of(
+            arguments(list(Integer.MAX_VALUE), list(Integer.MAX_VALUE, 0, Integer.MIN_VALUE), true),
+            arguments(list(Integer.MAX_VALUE, 0, Integer.MIN_VALUE), list(Integer.MAX_VALUE, 0, Integer.MIN_VALUE), false)
+        );
+    }
+
     @ParameterizedTest(name = "{1} | Increasing: {2}")
     @MethodSource
     @DisplayName("kinguinSort works for different input combinations")
@@ -24,15 +88,7 @@ public class KinguinSortTest extends TestBase {
             arguments(list(3, 4, 7, 9), list(3, 2, 4, 7, 1, 6, 5, 9, 8), true),
             arguments(list(3, 2, 1), list(3, 2, 4, 7, 1, 6, 5, 9, 8), false),
             arguments(list(-5, 0, 5), list(-5, 0, 5), true),
-            arguments(list(-5), list(-5, 0, 5), false),
-            arguments(list(0, 1), list(0, 1), true),
-            arguments(list(0), list(0, 1), false),
-            arguments(list(0), list(0), true),
-            arguments(list(0), list(0), false),
-            arguments(list(), list(), true),
-            arguments(list(), list(), false),
-            arguments(list(Integer.MAX_VALUE), list(Integer.MAX_VALUE, 0, Integer.MIN_VALUE), true),
-            arguments(list(Integer.MAX_VALUE, 0, Integer.MIN_VALUE), list(Integer.MAX_VALUE, 0, Integer.MIN_VALUE), false)
+            arguments(list(-5), list(-5, 0, 5), false)
         );
     }
 }
