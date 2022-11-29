064cb50afac9fad6eeadde0de36936414edfbdb4
Julian Hartl <90799563+julian-hartl@users.noreply.github.com>
2022-11-30 00:17:09 +0100

    [W06H03] Add further tests (#113)
    <!-- Danke, dass du deine Tests mit deinen Kommilitonen teilst. -->
    <!-- Beschreibe doch kurz hier drin, was deine Tests überprüfen und ob
    du dir eventuell bei etwas unsicher bist. -->
    <!-- Denk dran, dass deine Kommilitonen auch schon die Tests sehen,
    bevor dieser PR gemergt wird, pass also auf, was du an Code pusht!
    -->
    The new tests cover the following:
    - Negative amount of nodes (is possible)
    - Out of range calls on isAdj, getAdj and addEdge
    - Removal of isAdjFast test
    <!-- Bitte nutzte ebenfalls als Prefix für den Titel der Pull Request
    die Woche und die Nr. der Hausaufgabe, z.B "[W02H02] Deinen Titel".
    Solltest an keiner Hausaufgabe eine Änderung genommen haben, nutze
    stattdessen [*] als Prefix.
    
    Ebenfalls bitten wir dich darum, dass du nur an einer Hausaufgabe pro
    Pull Request Änderungen vornimmst. Solltest du Änderungen in mehreren
    Hausaufgaben vorgenommen haben, spalte bitte diese Änderungen in mehrere
    Pull Requests auf. -->
    
    Co-authored-by: Julian Hartl <julian.hartl04@gmail.com>

--- w06h03/test/pgdp/ds/behavior/SparseGraphTest.java
+++ w06h03/test/pgdp/ds/behavior/SparseGraphTest.java
@@ -4,10 +4,6 @@ import pgdp.ds.SparseGraph;
 
 public class SparseGraphTest extends GraphTest<SparseGraph> {
 
-    public SparseGraphTest() {
-        super(SparseGraph.class);
-    }
-
     @Override
     protected SparseGraph createGraph(int nodes) {
         return new SparseGraph(nodes);
