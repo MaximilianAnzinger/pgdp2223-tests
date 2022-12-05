93574567cbfb889dff33edf9b7195dac7cb15ef7
Banana <69007475+Friendly-Banana@users.noreply.github.com>
2022-12-06 00:16:54 +0100

    more tests

--- w07h03/pgdp/sim/SimulationTest.java
+++ w07h03/pgdp/sim/SimulationTest.java
@@ -17,12 +17,12 @@ public class SimulationTest {
         var a = new Simulation(cells, width, height);
         for (int i = 1; i < states.length; i++) {
             a.tick();
-            Assertions.assertEquals(states[i] + "\n", FieldVisualizeHelper.cellArrayToFieldDescription(a.getCells(), width, height, false), "State " + i);
+            Assertions.assertEquals(states[i] + "\n", FieldVisualizeHelper.cellArrayToFieldDescription(a.getCells(), width, height, false), "State " + i + ", " + (i + 1) + ".ter String");
         }
     }
 
     @Test
-    @DisplayName("One single plant only")
+    @DisplayName("One single plant")
     void singlePlant() {
         var states = new String[]{"""
                 g . . . .
@@ -52,6 +52,95 @@ public class SimulationTest {
                 . . . . ."""};
         SimConfig.plantMinGrowth = 1;
         SimConfig.plantMaxGrowth = 2;
+        SimConfig.plantReproductionCost = 0;
+        runTest("plant", 5, 5, states);
+    }
+
+    @Test
+    @DisplayName("single pingu, should reproduce and create big pingu")
+    void singlePingu() {
+        var states = new String[]{"""
+                p . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+                . . . . .""", """
+                p . . . .
+                p . . . .
+                . . . . .
+                . . . . .
+                . . . . .""", """
+                p p . . .
+                p . . . .
+                . . . . .
+                . . . . .
+                . . . . .""", """
+                p p p . .
+                p p . . .
+                p . . . .
+                . . . . .
+                . . . . .""", """
+                p p p p .
+                p p p . .
+                p p . . .
+                . p . . .
+                . . . . ."""};
+        SimConfig.pinguInitialFood = 3;
+        SimConfig.pinguFoodConsumption = 1;
+        SimConfig.pinguReproductionCost = 1;
+        runTest("plant", 5, 5, states);
+    }
+
+    @Test
+    void pinguEatsPlant() {
+        var states = new String[]{"""
+                p g . . .
+                . . . . .
+                . . . . .
+                . . . . .
+                . . . . .""", """
+                p . . . .
+                p . . . .
+                . . . . .
+                . . . . .
+                . . . . ."""};
+        SimConfig.pinguInitialFood = 1;
+        SimConfig.pinguConsumedFood = 2;
+        SimConfig.pinguFoodConsumption = 1;
+        SimConfig.pinguReproductionCost = 2;
+        runTest("plant", 5, 5, states);
+    }
+
+    @Test
+    @DisplayName("hamster eats plant, reproduces, wolf eats hamster child, reproduces")
+    void hamsterEatsPlantWolfEatsHamster() {
+        var states = new String[]{"""
+                h g . . .
+                . . . . .
+                w . . . .
+                . . . . .
+                . . . . .""", """
+                h . . . .
+                h . . . .
+                w . . . .
+                . . . . .
+                . . . . .""", """
+                . . . . .
+                . w . . .
+                . . . . .
+                w . . . .
+                . . . . ."""};
+        SimConfig.plantMinGrowth = 1;
+        SimConfig.plantMaxGrowth = 1;
+        SimConfig.plantReproductionCost = 3;
+        SimConfig.hamsterInitialFood = 1;
+        SimConfig.hamsterConsumedFood = 2;
+        SimConfig.hamsterFoodConsumption = 1;
+        SimConfig.hamsterReproductionCost = 3;
+        SimConfig.wolfInitialFood = 2;
+        SimConfig.wolfConsumedFood = 3;
+        SimConfig.wolfFoodConsumption = 1;
+        SimConfig.wolfReproductionCost = 3;
         runTest("plant", 5, 5, states);
     }
 }
