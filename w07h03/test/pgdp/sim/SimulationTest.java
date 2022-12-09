fc578216340d8537a2d29d7416b7c185f531cd68
Felix Hauptmann <git@felix-hauptmann.de>
2022-12-09 02:25:05 +0100

    [W07H03] access cells array via reflection and initialize SimConfig (#151)
    * we can not use getters which aren't mandatory according to the task
    description, therefore we have to use reflection
    * uninitialized `SimConfig` values could lead to confusing errors with
    faulty implmentations

--- w07h03/test/pgdp/sim/SimulationTest.java
+++ w07h03/test/pgdp/sim/SimulationTest.java
@@ -1,23 +1,55 @@
 package pgdp.sim;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Field;
 import java.nio.charset.StandardCharsets;
 
 public class SimulationTest {
-    void runTest(String seed, int width, int height, String[] states) {
-        runTest(seed.getBytes(StandardCharsets.UTF_8), width, height, states);
+
+    @BeforeEach
+    void setDefaults() {
+        SimConfig.plantReproductionCost = 0;
+        // has to be grater than 0 so in case of a faulty implementation rng does not throw an exception
+        SimConfig.plantMaxGrowth = 1;
+        SimConfig.plantMinGrowth = 0;
+        SimConfig.hamsterFoodConsumption = 0;
+        SimConfig.hamsterConsumedFood = 0;
+        SimConfig.hamsterReproductionCost = 0;
+        SimConfig.hamsterInitialFood = 0;
+        SimConfig.pinguFoodConsumption = 0;
+        SimConfig.pinguConsumedFood = 0;
+        SimConfig.pinguReproductionCost = 0;
+        SimConfig.pinguInitialFood = 0;
+        SimConfig.wolfFoodConsumption = 0;
+        SimConfig.wolfConsumedFood = 0;
+        SimConfig.wolfReproductionCost = 0;
+        SimConfig.wolfInitialFood = 0;
     }
 
-    void runTest(byte[] seed, int width, int height, String[] states) {
+    void runTest(String seed, int width, int height, String[] states) {
+        try {
+            runTest(seed.getBytes(StandardCharsets.UTF_8), width, height, states);
+        } catch (IllegalAccessException | NoSuchFieldException e) {
+            e.printStackTrace();
+        }
+    }
+
+    void runTest(byte[] seed, int width, int height, String[] states) throws IllegalAccessException, NoSuchFieldException {
         RandomGenerator.reseed(seed);
+
+        Field cellsField = Simulation.class.getDeclaredField("cells");
+        cellsField.setAccessible(true);
+
         var cells = FieldVisualizeHelper.fieldDescriptionToCellArray(states[0], width, height);
-        var a = new Simulation(cells, width, height);
+        var sim = new Simulation(cells, width, height);
         for (int i = 1; i < states.length; i++) {
-            a.tick();
-            Assertions.assertEquals(states[i] + "\n", FieldVisualizeHelper.cellArrayToFieldDescription(a.getCells(), width, height, false), "State " + i + ", " + (i + 1) + ".ter String");
+            sim.tick();
+            Cell[] currentCells = (Cell[]) cellsField.get(sim);
+            Assertions.assertEquals(states[i] + "\n", FieldVisualizeHelper.cellArrayToFieldDescription(currentCells, width, height, false), "State " + i + ", " + (i + 1) + ".ter String");
         }
     }
 
@@ -131,7 +163,7 @@ public class SimulationTest {
                 w . . . .
                 . . . . ."""};
         SimConfig.plantMinGrowth = 1;
-        SimConfig.plantMaxGrowth = 1;
+        SimConfig.plantMaxGrowth = 2;
         SimConfig.plantReproductionCost = 3;
         SimConfig.hamsterInitialFood = 1;
         SimConfig.hamsterConsumedFood = 2;
