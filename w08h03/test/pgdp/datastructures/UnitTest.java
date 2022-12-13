1c83534eff40f60cea98dbbef72ffef3920ab91b
Anatoly Weinstein <44678124+Anatoly03@users.noreply.github.com>
2022-12-13 21:02:36 +0100

    [W08H03] add generic and dynamic tests (#166)
    this pr introduces generic & generative tests for hard
    
    <!-- Danke, dass du deine Tests mit deinen Kommilitonen teilst. -->
    <!-- Beschreibe doch kurz hier drin, was deine Tests überprüfen und ob
    du dir eventuell bei etwas unsicher bist. -->
    <!-- Denk dran, dass deine Kommilitonen auch schon die Tests sehen,
    bevor dieser PR gemergt wird, pass also auf, was du an Code pusht!
    -->
    
    <!-- Bitte nutzte ebenfalls als Prefix für den Titel der Pull Request
    die Woche und die Nr. der Hausaufgabe, z.B "[W02H02] Deinen Titel".
    Solltest an keiner Hausaufgabe eine Änderung genommen haben, nutze
    stattdessen [*] als Prefix.
    
    Ebenfalls bitten wir dich darum, dass du nur an einer Hausaufgabe pro
    Pull Request Änderungen vornimmst. Solltest du Änderungen in mehreren
    Hausaufgaben vorgenommen haben, spalte bitte diese Änderungen in mehrere
    Pull Requests auf. -->

--- w08h03/test/pgdp/datastructures/UnitTest.java
+++ w08h03/test/pgdp/datastructures/UnitTest.java
@@ -2,8 +2,12 @@ package pgdp.datastructures;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.fail;
 
+import java.util.Arrays;
 import java.util.NoSuchElementException;
+import java.util.Random;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 import org.junit.jupiter.api.DisplayName;
@@ -14,48 +18,88 @@ import static org.junit.jupiter.params.provider.Arguments.arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 public class UnitTest {
-	@Test
-	public void emptyTest() {
-		assertThrows(NoSuchElementException.class, () -> (new QuarternarySearchTree<Integer>()).iterator().next());
-	}
+	static int seed = 69420;
 
-	@ParameterizedTest
-	@DisplayName("Dynamic Test")
-	@MethodSource
-	void dynamicTests(String type, int[] expected, int[] input) {
-		QuarternarySearchTree<Integer> tree = new QuarternarySearchTree<Integer>();
+	<Type extends Comparable<Type>> void testArray(Type[] input) {
+		QuarternarySearchTree<Type> tree = new QuarternarySearchTree<Type>();
+		Type[] expected = Arrays.copyOf(input, input.length);
+		Arrays.sort(expected);
 
-		for (int element : input) {
+		for (Type element : input) {
 			tree.insert(element);
 		}
 
 		int position = 0;
-		for (int element : tree) {
-			int exp = expected[position++];
-			assertEquals(exp, element, type + ": Invalid Output at position [" + (position-1) + "]: Expected [" + exp + "], got [" + element + "]");
+		for (Type element : tree) {
+			Type exp = expected[position++];
+			assertEquals(exp, element, "Invalid Output at position [" + (position - 1) + "]: Expected [" + exp
+					+ "], got [" + element + "]");
 		}
 
-		assertEquals(expected.length, position, type + ": Invalid Iteration Count. Expected [" + expected.length + "] got [" + position + "]");
+		assertEquals(expected.length, position,
+				"Invalid Iteration Count. Expected [" + expected.length + "] got [" + position + "]");
 	}
 
-	private static Stream<Arguments> dynamicTests() {
-		// <region tests>
-		return Stream.of(
-				//
-				// Artemis Example
-				//
-				arguments(
-						"Artemis Example",
-						new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 },
-						new int[] { 8, 4, 12, 1, 5, 9, 13, 3, 7, 11, 15, 2, 6, 10, 14 }),
-				//
-				// Empty Example
-				//
-				arguments(
-						"Empty Test",
-						new int[] {},
-						new int[] {})
-		// </end-region>
-		);
+	@Test
+	@DisplayName("should iterate over empty graph")
+	public void emptyTest() {
+		assertThrows(NoSuchElementException.class, () -> (new QuarternarySearchTree<Integer>()).iterator().next());
+	}
+
+	@Test
+	@DisplayName("should iterate over graph in artemis example")
+	public void artemisTest() {
+		testArray(new Integer[] { 8, 4, 12, 1, 5, 9, 13, 3, 7, 11, 15, 2, 6, 10, 14 });
+	}
+
+	@Test
+	@DisplayName("should iterate over a graph of one repeat element")
+	public void oneElementTest() {
+		testArray(new Integer[] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 });
+	}
+
+	@Test
+	@DisplayName("should iterate over a numeric graph")
+	public void numericTest() {
+		IntStream.range(0, 10).forEach(i -> {
+			var args = new Integer[i];
+			var rnd = new Random(seed + i);
+
+			for (int j = 0; j < args.length; j++) {
+				args[j] = rnd.nextInt(Integer.MIN_VALUE, Integer.MAX_VALUE);
+			}
+
+			testArray(args);
+		});
+	}
+
+	// private static int seedDifference = 0;
+	// private static Stream<Arguments> testArray() {
+	// 	// TODO improve this
+	// 	return Stream.generate(() -> {
+	// 		return arguments((new Random(seed + seedDifference++)).ints(seedDifference, 0, Integer.MAX_VALUE).toArray());
+	// 	}).limit(10);
+	// }
+
+	private String generateString(Random rnd) {
+		StringBuilder s = new StringBuilder();
+		rnd.ints(rnd.nextInt(100), 97, 122).mapToObj(i -> (char) i).forEach(c -> s.append(c));
+		return s.toString();
+	}
+
+	@Test
+	@DisplayName("should iterate over a non-numeric graph")
+	public void stringTest() {
+		IntStream.range(0, 10).forEach(i -> {
+			var args = new String[i];
+			var rnd = new Random(seed + i);
+
+			for (int j = 0; j < args.length; j++) {
+				args[j] = generateString(rnd);
+				// System.out.println(args[j]);
+			}
+
+			testArray(args);
+		});
 	}
 }
