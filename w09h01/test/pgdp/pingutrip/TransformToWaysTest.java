3ca8b356ee2c83d8259ab52d7074a73202f03279
nilsreichardt <hi@nils.re>
2022-12-17 12:41:40 +0100

    Use points from array

--- w09h01/test/pgdp/pingutrip/TransformToWaysTest.java
+++ w09h01/test/pgdp/pingutrip/TransformToWaysTest.java
@@ -12,7 +12,7 @@ public class TransformToWaysTest {
         List<WayPoint> points = List.of(new WayPoint(4.0, 11.5), new WayPoint(19.1, 3.2));
         List<OneWay> ways = PinguTrip.transformToWays(points).toList();
 
-        OneWay[] expected = { new OneWay(new WayPoint(4.0, 11.5), new WayPoint(19.1, 3.2)) };
+        OneWay[] expected = { new OneWay(points.get(0), points.get(1)) };
         OneWay[] actual = ways.toArray(OneWay[]::new);
 
         assertEquals(expected.length, actual.length,
