9dc0fcb67749bb89eb58677036f65c74e9209366
JGStyle <jgs@jws.de>
2022-12-21 12:59:05 +0100

    Converted all official Examples from the main method to JUnit Tests

---
+++ w09h02/test/pgdp/trains/processing/OfficialExampleTests.java
@@ -0,0 +1,135 @@
+package pgdp.trains.processing;
+
+import org.junit.jupiter.api.Test;
+import pgdp.trains.connections.Station;
+import pgdp.trains.connections.TrainConnection;
+import pgdp.trains.connections.TrainStop;
+
+import java.time.LocalDateTime;
+import java.util.*;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class OfficialExampleTests {
+
+    List<TrainConnection> trainConnections = List.of(
+            new TrainConnection("ICE 2", "ICE", "2", "DB", List.of(
+                    new TrainStop(Station.MUENCHEN_HBF,
+                            LocalDateTime.of(2022, 12, 1, 11, 0),
+                            LocalDateTime.of(2022, 12, 1, 11, 0),
+                            TrainStop.Kind.REGULAR),
+                    new TrainStop(Station.NUERNBERG_HBF,
+                            LocalDateTime.of(2022, 12, 1, 11, 30),
+                            LocalDateTime.of(2022, 12, 1, 12, 0),
+                            TrainStop.Kind.REGULAR)
+            )),
+            new TrainConnection("ICE 1", "ICE", "1", "DB", List.of(
+                    new TrainStop(Station.MUENCHEN_HBF,
+                            LocalDateTime.of(2022, 12, 1, 10, 0),
+                            LocalDateTime.of(2022, 12, 1, 10, 0),
+                            TrainStop.Kind.REGULAR),
+                    new TrainStop(Station.NUERNBERG_HBF,
+                            LocalDateTime.of(2022, 12, 1, 10, 30),
+                            LocalDateTime.of(2022, 12, 1, 10, 30),
+                            TrainStop.Kind.REGULAR)
+            )),
+            new TrainConnection("ICE 3", "ICE", "3", "DB", List.of(
+                    new TrainStop(Station.MUENCHEN_HBF,
+                            LocalDateTime.of(2022, 12, 1, 12, 0),
+                            LocalDateTime.of(2022, 12, 1, 12, 0),
+                            TrainStop.Kind.REGULAR),
+                    new TrainStop(Station.AUGSBURG_HBF,
+                            LocalDateTime.of(2022, 12, 1, 12, 20),
+                            LocalDateTime.of(2022, 12, 1, 13, 0),
+                            TrainStop.Kind.CANCELLED),
+                    new TrainStop(Station.NUERNBERG_HBF,
+                            LocalDateTime.of(2022, 12, 1, 13, 30),
+                            LocalDateTime.of(2022, 12, 1, 13, 30),
+                            TrainStop.Kind.REGULAR)
+            ))
+    );
+
+    public static <T> void assertStreamEquals(Stream<T> expected, Stream<T> actual) {
+        Iterator<T> expectedIterator = expected.iterator(), actualIterator = actual.iterator();
+        while (expectedIterator.hasNext() && actualIterator.hasNext())
+            assertEquals(expectedIterator.next(), actualIterator.next());
+        assert !expectedIterator.hasNext() && !actualIterator.hasNext();
+    }
+
+    @Test
+    void cleanDataset() {
+        List<TrainConnection> cleanedConnections = List.of(
+                new TrainConnection("ICE 1", "ICE", "1", "DB", List.of(
+                        new TrainStop(Station.MUENCHEN_HBF,
+                                LocalDateTime.of(2022, 12, 1, 10, 0),
+                                LocalDateTime.of(2022, 12, 1, 10, 0),
+                                TrainStop.Kind.REGULAR),
+                        new TrainStop(Station.NUERNBERG_HBF,
+                                LocalDateTime.of(2022, 12, 1, 10, 30),
+                                LocalDateTime.of(2022, 12, 1, 10, 30),
+                                TrainStop.Kind.REGULAR)
+                )),
+                new TrainConnection("ICE 2", "ICE", "2", "DB", List.of(
+                        new TrainStop(Station.MUENCHEN_HBF,
+                                LocalDateTime.of(2022, 12, 1, 11, 0),
+                                LocalDateTime.of(2022, 12, 1, 11, 0),
+                                TrainStop.Kind.REGULAR),
+                        new TrainStop(Station.NUERNBERG_HBF,
+                                LocalDateTime.of(2022, 12, 1, 11, 30),
+                                LocalDateTime.of(2022, 12, 1, 12, 0),
+                                TrainStop.Kind.REGULAR)
+                )),
+                new TrainConnection("ICE 3", "ICE", "3", "DB", List.of(
+                        new TrainStop(Station.MUENCHEN_HBF,
+                                LocalDateTime.of(2022, 12, 1, 12, 0),
+                                LocalDateTime.of(2022, 12, 1, 12, 0),
+                                TrainStop.Kind.REGULAR),
+                        new TrainStop(Station.NUERNBERG_HBF,
+                                LocalDateTime.of(2022, 12, 1, 13, 30),
+                                LocalDateTime.of(2022, 12, 1, 13, 30),
+                                TrainStop.Kind.REGULAR)
+                ))
+        );
+        assertStreamEquals(cleanedConnections.stream(), DataProcessing.cleanDataset(trainConnections.stream()));
+    }
+
+    @Test
+    void worstDelayedTrain() {
+        assertEquals(trainConnections.get(2), DataProcessing.worstDelayedTrain(trainConnections.stream()));
+    }
+
+    @Test
+    void percentOfKindStopsWithKindRegular() {
+        assertEquals(85.71428571428571, DataProcessing.percentOfKindStops(trainConnections.stream(), TrainStop.Kind.REGULAR));
+    }
+
+    @Test
+    void percentOfKindStopsWithKindCancelled() {
+        assertEquals(14.285714285714285, DataProcessing.percentOfKindStops(trainConnections.stream(), TrainStop.Kind.CANCELLED));
+    }
+
+    @Test
+    void averageDelayAt() {
+        assertEquals(10.0, DataProcessing.averageDelayAt(trainConnections.stream(), Station.NUERNBERG_HBF));
+    }
+
+    @Test
+    void delayComparedToTotalTravelTimeByTransport() {
+        Map<String, Double> actual = DataProcessing.delayComparedToTotalTravelTimeByTransport(trainConnections.stream());
+        assertEquals(16.666666666666668, actual.get("ICE"));
+    }
+
+    @Test
+    void averageDelayByHour() {
+        Map<Integer, Double> actual = DataProcessing.averageDelayByHour(trainConnections.stream());
+        assertEquals(0.0, actual.get(10));
+        assertEquals(0.0, actual.get(11));
+        assertEquals(15.0, actual.get(12));
+        assertEquals(20.0, actual.get(13));
+        Set<Integer> expectedKeySet = Set.of(10, 11, 12, 13);
+        assertEquals(expectedKeySet, actual.keySet());
+    }
+
+}
