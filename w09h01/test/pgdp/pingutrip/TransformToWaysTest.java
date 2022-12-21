7f6f512c7f0ecb11dc9a7fa6b6d31816ed9f8f5a
nilsreichardt <hi@nils.re>
2022-12-21 12:11:31 +0100

    Use `assertArrayEquals`

--- w09h01/test/pgdp/pingutrip/TransformToWaysTest.java
+++ w09h01/test/pgdp/pingutrip/TransformToWaysTest.java
@@ -1,6 +1,6 @@
 package pgdp.pingutrip;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 import java.util.List;
 
@@ -9,16 +9,13 @@ import org.junit.jupiter.api.Test;
 public class TransformToWaysTest {
     @Test
     void testTransformToWays() {
-        List<WayPoint> points = List.of(new WayPoint(4.0, 11.5), new WayPoint(19.1, 3.2));
+        List<WayPoint> points = List.of(new WayPoint(4.0, 11.5), new WayPoint(19.1, 3.2), new WayPoint(2.1, 7.4));
         List<OneWay> ways = PinguTrip.transformToWays(points).toList();
 
-        OneWay[] expected = { new OneWay(points.get(0), points.get(1)) };
+        OneWay[] expected = { new OneWay(new WayPoint(4.0, 11.5), new WayPoint(19.1, 3.2)),
+                new OneWay(new WayPoint(19.1, 3.2), new WayPoint(2.1, 7.4)) };
         OneWay[] actual = ways.toArray(OneWay[]::new);
 
-        assertEquals(expected.length, actual.length,
-                "The stream should contain exactly " + expected.length + " values");
-        for (int i = 0; i < expected.length; i++) {
-            assertEquals(expected[i], actual[i], "The " + i + ". element is wrong");
-        }
+        assertArrayEquals(expected, actual);
     }
 }
