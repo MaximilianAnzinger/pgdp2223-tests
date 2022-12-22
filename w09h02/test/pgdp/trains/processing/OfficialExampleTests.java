2b44ad7864e949bb92da695602e526320124979d
JGStyle <jgs@jws.de>
2022-12-22 11:38:30 +0100

    fix wrong commit; added epsilon value to all double assertions

--- w09h02/test/pgdp/trains/processing/OfficialExampleTests.java
+++ w09h02/test/pgdp/trains/processing/OfficialExampleTests.java
@@ -1,7 +1,3 @@
-Last login: Thu Dec 22 11:29:41 on ttys000
-❯ ls
- Applications   Documents   Library   Music      Public
- Desktop        Downloads   Movies    Pictures   PycharmProjects
 package pgdp.trains.processing;
 
 import org.junit.jupiter.api.Test;
@@ -18,29 +14,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class OfficialExampleTests {
 
-    List<TrainConnection> trainConnections = List.of(
-            new TrainConnection("ICE 2", "ICE", "2", "DB", List.of(
-                    new TrainStop(Station.MUENCHEN_HBF,
-                            LocalDateTime.of(2022, 12, 1, 11, 0),
-                            LocalDateTime.of(2022, 12, 1, 11, 0),
-                            TrainStop.Kind.REGULAR),
-                    new TrainStop(Station.NUERNBERG_HBF,
-                            LocalDateTime.of(2022, 12, 1, 11, 30),
--- EINFÜGEN --package pgdp.trains.processing;
-
-import org.junit.jupiter.api.Test;
-import pgdp.trains.connections.Station;
-import pgdp.trains.connections.TrainConnection;
-import pgdp.trains.connections.TrainStop;
-
-import java.time.LocalDateTime;
-import java.util.*;
-import java.util.stream.Stream;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-
-class OfficialExampleTests {
+    private final double EPSILON = 0.00001;
 
     List<TrainConnection> trainConnections = List.of(
             new TrainConnection("ICE 2", "ICE", "2", "DB", List.of(
@@ -130,32 +104,32 @@ class OfficialExampleTests {
 
     @Test
     void percentOfKindStopsWithKindRegular() {
-        assertEquals(85.71428571428571, DataProcessing.percentOfKindStops(trainConnections.stream(), TrainStop.Kind.REGULAR), 0.00001);
+        assertEquals(85.71428571428571, DataProcessing.percentOfKindStops(trainConnections.stream(), TrainStop.Kind.REGULAR), EPSILON);
     }
 
     @Test
     void percentOfKindStopsWithKindCancelled() {
-        assertEquals(14.285714285714285, DataProcessing.percentOfKindStops(trainConnections.stream(), TrainStop.Kind.CANCELLED), 0.00001);
+        assertEquals(14.285714285714285, DataProcessing.percentOfKindStops(trainConnections.stream(), TrainStop.Kind.CANCELLED), EPSILON);
     }
 
     @Test
     void averageDelayAt() {
-        assertEquals(10.0, DataProcessing.averageDelayAt(trainConnections.stream(), Station.NUERNBERG_HBF));
+        assertEquals(10.0, DataProcessing.averageDelayAt(trainConnections.stream(), Station.NUERNBERG_HBF), EPSILON);
     }
 
     @Test
     void delayComparedToTotalTravelTimeByTransport() {
         Map<String, Double> actual = DataProcessing.delayComparedToTotalTravelTimeByTransport(trainConnections.stream());
-        assertEquals(16.666666666666668, actual.get("ICE"), 0.00001);
+        assertEquals(16.666666666666668, actual.get("ICE"),  EPSILON);
     }
 
     @Test
     void averageDelayByHour() {
         Map<Integer, Double> actual = DataProcessing.averageDelayByHour(trainConnections.stream());
-        assertEquals(0.0, actual.get(10));
-        assertEquals(0.0, actual.get(11));
-        assertEquals(15.0, actual.get(12));
-        assertEquals(20.0, actual.get(13));
+        assertEquals(0.0, actual.get(10), EPSILON);
+        assertEquals(0.0, actual.get(11), EPSILON);
+        assertEquals(15.0, actual.get(12), EPSILON);
+        assertEquals(20.0, actual.get(13), EPSILON);
         Set<Integer> expectedKeySet = Set.of(10, 11, 12, 13);
         assertEquals(expectedKeySet, actual.keySet());
     }
