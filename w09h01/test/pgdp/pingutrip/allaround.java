18a93d32fd34a75c5c1525d276972c59c9abcc94
Benn1x <87654022+Benn1x@users.noreply.github.com>
2022-12-31 16:28:44 +0100

    Some utility tests for `kidFriendlyTrip()` and `pathLenghtSimple()` and other

--- w09h01/test/pgdp/pingutrip/allaround.java
+++ w09h01/test/pgdp/pingutrip/allaround.java
@@ -29,11 +29,34 @@ public class AllAround {
         assertArrayEquals(PinguTrip.kidFriendlyTrip(lists), result);
     }
 
+    @Test
+    void transformToWaysTest(){
+        List<WayPoint> list = List.of(new WayPoint(4.0, 11.5), new WayPoint(19.1, 3.2), new WayPoint(2.1, 7.4));
+        assertArrayEquals(PinguTrip.transformToWays(list).toList(),List.of(new OneWay(new WayPoint(4.0, 11.5), new WayPoint(19.1, 3.2)), new OneWay(new WayPoint(19.1, 3.2), new WayPoint(2.1, 7.4))));
+    }
+
+    @Test
+    void transformToWaysTestVoid(){
+        List<WayPoint> list = List.of();
+        assertArrayEquals(PinguTrip.transformToWays(list).toList(),List.of());
+    }
+
+    @Test
+    void furthestAWay(){
+        Stream<WayPoint> path = Stream.of(new WayPoint(1.0, 0.0), new WayPoint(2.0, 0.0));
+        WayPoint home = new WayPoint(0.0, 0.0);
+        assertArrayEquals(PinguTrip.furthestAwayFromHome(path, home), new WayPoint(2.0, 0.0));
+    }
+
+    private void assertArrayEquals(WayPoint furthestAwayFromHome, WayPoint wayPoint) {
+        if(!furthestAwayFromHome.equals(wayPoint)) fail("They dont match expected:"+ wayPoint +" but was: "+ furthestAwayFromHome);
+    }
+
     private void assertArrayEquals(List<OneWay> kidFriendlyTrip, List<OneWay> result) {
-        if(kidFriendlyTrip.size() != result.size()) fail("Unequal already in size");
+        if(kidFriendlyTrip.size() != result.size()) fail("Unequal already in size. Expected: "+result.size()+" but were: "+kidFriendlyTrip.size());
         else{
             for (int i = 0; i < kidFriendlyTrip.size(); i++) {
-                if(!kidFriendlyTrip.get(i).equals(result.get(i))) fail("Unequal at element:"+i+" your were: "+kidFriendlyTrip.get(i)+" the expectet where: "+result.get(i));
+                if(!kidFriendlyTrip.get(i).equals(result.get(i))) fail("Unequal at element: "+i+" your were: "+kidFriendlyTrip.get(i)+" the expectet where: "+result.get(i));
             }
         }
     }
