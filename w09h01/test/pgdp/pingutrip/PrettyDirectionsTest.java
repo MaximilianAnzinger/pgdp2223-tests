47d8c8174cfef62b22b2bd00ce597f9a97649acf
Nils Reichardt <me@nils.re>
2023-01-03 19:21:26 +0100

    [W09H01] Add test for `prettyDirections` (task 7) (#215)
    Adding the example from Artemis.

---
+++ w09h01/test/pgdp/pingutrip/PrettyDirectionsTest.java
@@ -0,0 +1,23 @@
+package pgdp.pingutrip;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.Test;
+
+public class PrettyDirectionsTest {
+    @Test
+    void testIsOnWayTest() {
+        Stream<OneWay> ways = Stream.of(new OneWay(new WayPoint(0.0, 0.0), new WayPoint(1.0, 0.0)),
+                new OneWay(new WayPoint(1.0, 0.0), new WayPoint(3.0, 0.0)),
+                new OneWay(new WayPoint(3.0, 0.0), new WayPoint(4.0, 1.0)));
+
+        String output = PinguTrip.prettyDirections(ways);
+
+        assertEquals("""
+                1 Schritte Richtung 0 Grad.
+                3 Schritte Richtung 0 Grad.
+                2 Schritte Richtung 45 Grad.""", output);
+    }
+}
