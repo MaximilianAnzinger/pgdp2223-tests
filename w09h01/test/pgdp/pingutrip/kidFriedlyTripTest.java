2b07ebedf03cc425ff8d465c4c65ed0e94d8a3ba
Benn1x <87654022+Benn1x@users.noreply.github.com>
2023-01-04 22:06:07 +0100

    Update w09h01/test/pgdp/pingutrip/kidFriedlyTripTest.java
    Co-authored-by: Nils Reichardt <me@nils.re>

--- w09h01/test/pgdp/pingutrip/kidFriedlyTripTest.java
+++ w09h01/test/pgdp/pingutrip/kidFriedlyTripTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 public class kidFriedlyTripTest {
     @Test
