8a7f17e2fffc876c3678664c4cd044cc1867ff9e
nilsreichardt <hi@nils.re>
2023-01-04 10:52:32 +0100

    Remove tests for `percentOfKindStops` in `OfficialExampleTests.java`

--- w09h02/test/pgdp/trains/processing/OfficialExampleTests.java
+++ w09h02/test/pgdp/trains/processing/OfficialExampleTests.java
@@ -104,16 +104,6 @@ class OfficialExampleTests {
     }
 
     @Test
-    void percentOfKindStopsWithKindRegular() {
-        assertEquals(85.71428571428571, DataProcessing.percentOfKindStops(trainConnections.stream(), TrainStop.Kind.REGULAR), EPSILON);
-    }
-
-    @Test
-    void percentOfKindStopsWithKindCancelled() {
-        assertEquals(14.285714285714285, DataProcessing.percentOfKindStops(trainConnections.stream(), TrainStop.Kind.CANCELLED), EPSILON);
-    }
-
-    @Test
     void averageDelayAt() {
         assertEquals(10.0, DataProcessing.averageDelayAt(trainConnections.stream(), Station.NUERNBERG_HBF), EPSILON);
     }
