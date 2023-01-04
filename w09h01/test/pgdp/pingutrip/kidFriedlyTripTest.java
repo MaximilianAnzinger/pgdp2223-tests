09897224974d0400119c795a95c50749366aa9d0
Benn1x <87654022+Benn1x@users.noreply.github.com>
2023-01-04 14:06:55 +0100

    Update w09h01/test/pgdp/pingutrip/kidFriedlyTripTest.java
    Co-authored-by: Nils Reichardt <me@nils.re>

--- w09h01/test/pgdp/pingutrip/kidFriedlyTripTest.java
+++ w09h01/test/pgdp/pingutrip/kidFriedlyTripTest.java
@@ -20,14 +20,17 @@ public class kidFriedlyTripTest {
 
     @Test
     @DisplayName("Checks if every element gets returned, checks for the correctness of the math")
-    void kidFriendlyTripTest2(){
+    void kidFriendlyTripTest2() {
         List<OneWay> lists = List.of(new OneWay(new WayPoint(2.0, 0.0), new WayPoint(4.0, 0.0)),
                 new OneWay(new WayPoint(0.0, 0.0), new WayPoint(2.0, 0.0)),
                 new OneWay(new WayPoint(0.0, 0.0), new WayPoint(2.0, 0.0)));
-        List<OneWay> result = List.of(new OneWay(new WayPoint(2.0, 0.0), new WayPoint(4.0, 0.0)),
+
+        List<OneWay> expected = List.of(new OneWay(new WayPoint(2.0, 0.0), new WayPoint(4.0, 0.0)),
                 new OneWay(new WayPoint(0.0, 0.0), new WayPoint(2.0, 0.0)),
                 new OneWay(new WayPoint(0.0, 0.0), new WayPoint(2.0, 0.0)));
-        assertArrayEquals(PinguTrip.kidFriendlyTrip(lists), result);
+        List<OneWay> actual = PinguTrip.kidFriendlyTrip(lists);
+
+        assertArrayEquals(expected.toArray(), actual.toArray());
     }
 
     private void assertArrayEquals(WayPoint furthestAwayFromHome, WayPoint wayPoint) {
