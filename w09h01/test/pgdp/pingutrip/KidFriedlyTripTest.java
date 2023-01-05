28563782e04dbd2f64f9fb56862d6c9e05e30040
nilsreichardt <hi@nils.re>
2023-01-05 12:39:32 +0100

    Refactor `KidFriedlyTripTest.java`

--- w09h01/test/pgdp/pingutrip/KidFriedlyTripTest.java
+++ w09h01/test/pgdp/pingutrip/KidFriedlyTripTest.java
@@ -1,16 +1,16 @@
 package pgdp.pingutrip;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 public class KidFriedlyTripTest {
     @Test
     @DisplayName("Checks if really stops in kidFriendlyTripTest when the first element is longer then the average ")
-    void kidFriendlyTripTest() {
+    void testKidFriendlyTrip() {
         List<OneWay> lists = List.of(new OneWay(new WayPoint(0.0, 0.0), new WayPoint(4.0, 0.0)),
                 new OneWay(new WayPoint(0.0, 0.0), new WayPoint(1.0, 0.0)),
                 new OneWay(new WayPoint(1.0, 0.0), new WayPoint(3.0, 0.0)));
@@ -23,7 +23,7 @@ public class KidFriedlyTripTest {
 
     @Test
     @DisplayName("Checks if every element gets returned, checks for the correctness of the math")
-    void kidFriendlyTripTest2() {
+    void testKidFriendlyTrip2() {
         List<OneWay> lists = List.of(new OneWay(new WayPoint(2.0, 0.0), new WayPoint(4.0, 0.0)),
                 new OneWay(new WayPoint(0.0, 0.0), new WayPoint(2.0, 0.0)),
                 new OneWay(new WayPoint(0.0, 0.0), new WayPoint(2.0, 0.0)));
@@ -34,18 +34,5 @@ public class KidFriedlyTripTest {
         List<OneWay> actual = PinguTrip.kidFriendlyTrip(lists);
 
         assertArrayEquals(expected.toArray(), actual.toArray());
-    }
-
-    private void assertArrayEquals(WayPoint furthestAwayFromHome, WayPoint wayPoint) {
-        if(!furthestAwayFromHome.equals(wayPoint)) fail("They dont match expected:"+ wayPoint +" but was: "+ furthestAwayFromHome);
-    }
-
-    private void assertArrayEquals(List<OneWay> kidFriendlyTrip, List<OneWay> result) {
-        if(kidFriendlyTrip.size() != result.size()) fail("Unequal already in size. Expected: "+result.size()+" but were: "+kidFriendlyTrip.size());
-        else{
-            for (int i = 0; i < kidFriendlyTrip.size(); i++) {
-                if(!kidFriendlyTrip.get(i).equals(result.get(i))) fail("Unequal at element: "+i+" your were: "+kidFriendlyTrip.get(i)+" the expectet where: "+result.get(i));
-            }
-        }
     }
 }
