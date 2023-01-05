3a656f69257124b34c19784468919189fe655b2e
nilsreichardt <hi@nils.re>
2023-01-05 12:38:21 +0100

    Refactor `FurthestAwayFromHomeTest.java`

--- w09h01/test/pgdp/pingutrip/FurthestAwayFromHomeTest.java
+++ w09h01/test/pgdp/pingutrip/FurthestAwayFromHomeTest.java
@@ -1,17 +1,16 @@
 package pgdp.pingutrip;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.stream.Stream;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 public class FurthestAwayFromHomeTest {
     @Test
     @DisplayName("The Artemis tests")
-    void FurthestAwayFromHomeTest() {
+    void testExample() {
         Stream<WayPoint> path = Stream.of(new WayPoint(1.0, 0.0), new WayPoint(2.0, 0.0));
         WayPoint home = new WayPoint(0.0, 0.0);
         WayPoint actual = PinguTrip.furthestAwayFromHome(path, home);
@@ -19,35 +18,41 @@ public class FurthestAwayFromHomeTest {
         assertEquals(expected, actual);
     }
 
-
     @Test
-    @DisplayName("With to identical biggest element ")
-    void FurthestAwayFromHomeTest1(){
-        Stream<WayPoint> path = Stream.of(new WayPoint(3.0, 4.0), new WayPoint(3.0, 4.1) ,new WayPoint(1.0, 0.0), new WayPoint(2.0, 0.0));
+    @DisplayName("With to identical biggest element")
+    void testWithIdenticalBiggestElement() {
+        Stream<WayPoint> path = Stream.of(new WayPoint(3.0, 4.0), new WayPoint(3.0, 4.1), new WayPoint(1.0, 0.0),
+                new WayPoint(2.0, 0.0));
         WayPoint home = new WayPoint(0.0, 0.0);
-        WayPoint result = PinguTrip.furthestAwayFromHome(path, home);
+
+        WayPoint actual = PinguTrip.furthestAwayFromHome(path, home);
         WayPoint expected = new WayPoint(3.0, 4.1);
-        assertEquals(result, expected);
+
+        assertEquals(expected, actual);
     }
 
     @Test
     @DisplayName("Stream with no elements should return home")
-    void FurthestAwayFromHomeTest2(){
+    void testWithEmptyStream() {
         Stream<WayPoint> path = Stream.of();
         WayPoint home = new WayPoint(0.0, 0.0);
-        WayPoint result = PinguTrip.furthestAwayFromHome(path, home);
+
+        WayPoint actual = PinguTrip.furthestAwayFromHome(path, home);
         WayPoint expected = new WayPoint(0.0, 0.0);
-        assertEquals(result, expected);
+
+        assertEquals(expected, actual);
     }
 
     @Test
-    @DisplayName("Now with a home that is not a 0.0")
-    void FurthestAwayFromHomeTest3(){
-        Stream<WayPoint> path = Stream.of(new WayPoint(3.0, 4.1), new WayPoint(4.1, 3.0) ,new WayPoint(1.0, 0.0), new WayPoint(2.0, 0.0));
+    @DisplayName("Home that is not a 0.0")
+    void testHomeIsZero() {
+        Stream<WayPoint> path = Stream.of(new WayPoint(3.0, 4.1), new WayPoint(4.1, 3.0), new WayPoint(1.0, 0.0),
+                new WayPoint(2.0, 0.0));
         WayPoint home = new WayPoint(4.1, 3.0);
-        WayPoint result = PinguTrip.furthestAwayFromHome(path, home);
-        WayPoint expected =  new WayPoint(1.0, 0.0);
-        assertEquals(result ,expected);
-    }
 
+        WayPoint actual = PinguTrip.furthestAwayFromHome(path, home);
+        WayPoint expected = new WayPoint(1.0, 0.0);
+
+        assertEquals(expected, actual);
+    }
 }
