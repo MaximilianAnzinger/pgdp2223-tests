a989b45e2972502bedd002b978077372cc0d57b6
Nils Reichardt <me@nils.re>
2023-01-07 15:17:29 +0100

    [W09H02] Add tests for `percentOfKindStops` (#217)
    * Moves the example test from `OfficialExampleTests.java` into a
    separate file `PercentOfKindStopsTest.java`
    * Add a test for every `TrainKind` where the percent of stops is 100%
    * Add a test for every `TrainKind` where the percent of stops is 0%
    * Add a test for every `TrainKind` where the list of trains connections
    is empty

--- w09h02/test/pgdp/trains/processing/OfficialExampleTests.java
+++ w09h02/test/pgdp/trains/processing/OfficialExampleTests.java
@@ -99,16 +99,6 @@ class OfficialExampleTests {
     }
 
     @Test
-    void percentOfKindStopsWithKindRegular() {
-        assertEquals(85.71428571428571, DataProcessing.percentOfKindStops(trainConnections.stream(), TrainStop.Kind.REGULAR), EPSILON);
-    }
-
-    @Test
-    void percentOfKindStopsWithKindCancelled() {
-        assertEquals(14.285714285714285, DataProcessing.percentOfKindStops(trainConnections.stream(), TrainStop.Kind.CANCELLED), EPSILON);
-    }
-
-    @Test
     void delayComparedToTotalTravelTimeByTransport() {
         Map<String, Double> actual = DataProcessing.delayComparedToTotalTravelTimeByTransport(trainConnections.stream());
         assertEquals(16.666666666666668, actual.get("ICE"),  EPSILON);
