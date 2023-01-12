ea96624a3bcfa3c6f3be94132b8048964be10038
MuellerMarlon <marlon130403@gmail.com>
2023-01-12 09:53:20 +0100

    Fix

--- w10h01/test/pgdp/trials/TrialOfTheDreamsTest.java
+++ w10h01/test/pgdp/trials/TrialOfTheDreamsTest.java
@@ -8,14 +8,14 @@ import java.util.function.Function;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TrialOfTheDreamsTest {
-    private Function<byte[], Boolean> getLock() {
+    private Function<byte[], Boolean> getLock(byte[] combination) {
         return (byte[] bytes) -> {
             if (bytes.length != combination.length) {return false;}
             for (int i = 0; i < bytes.length; i++) {
                 if (bytes[i] != combination[i]) {return false;}
             }
-            return true; 
-        }
+            return true;
+        };
     }
 
     @Test
@@ -35,7 +35,7 @@ public class TrialOfTheDreamsTest {
     @DisplayName("Combination of Length 2")
     void TestLengthTwo() {
         byte[] combination = {-128, 127};
-        Function<byte[], Boolean> lock = getLock();
+        Function<byte[], Boolean> lock = getLock(combination);
 
         byte[] result = TrialOfTheDreams.lockPick(lock);
 
@@ -48,7 +48,7 @@ public class TrialOfTheDreamsTest {
     @DisplayName("Combination of Length 3")
     void TestLengthThree() {
         byte[] combination = {25, 8, 30};
-        Function<byte[], Boolean> lock = getLock();
+        Function<byte[], Boolean> lock = getLock(combination);
 
         byte[] result = TrialOfTheDreams.lockPick(lock);
 
@@ -61,21 +61,20 @@ public class TrialOfTheDreamsTest {
     @DisplayName("Combination of Length 3 with negative numbers")
     void testLengthThreeNegative() {
         byte[] combination = {25, -8, 30};
-        Function<byte[], Boolean> lock = getLock();
-        };
+        Function<byte[], Boolean> lock = getLock(combination);
 
         byte[] result = TrialOfTheDreams.lockPick(lock);
 
-        for (int i = 0; i < combination.length; i++) {
+            for(int i = 0; i < combination.length; i++) {
             assertEquals(combination[i], result[i]);
         }
-    }
+}
 
     @Test
     @DisplayName("Empty Array")
     void testEmpty() {
         byte[] combination = new byte[0];
-        Function<byte[], Boolean> lock = getLock();
+        Function<byte[], Boolean> lock = getLock(combination);
 
         byte[] result = TrialOfTheDreams.lockPick(lock);
 
@@ -84,4 +83,3 @@ public class TrialOfTheDreamsTest {
         }
     }
 }
-
