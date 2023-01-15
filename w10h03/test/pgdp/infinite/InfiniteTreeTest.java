48358c2629f1fa1ce9221e44350d095a77b3eff4
yassine <ge87won@mytum.de>
2023-01-15 01:20:39 +0100

    Added Tests for stopping immediately after optimizable.process(value) is true

--- w10h03/test/pgdp/infinite/InfiniteTreeTest.java
+++ w10h03/test/pgdp/infinite/InfiniteTreeTest.java
@@ -1,16 +1,50 @@
 package pgdp.infinite;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import pgdp.infinite.tree.InfiniteTree;
+import pgdp.infinite.tree.Optimizable;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Function;
+import java.util.function.Supplier;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 public class InfiniteTreeTest {
+
+    public void binaryTreeHelper(InfiniteTree<Long> binaryValueTree){
+        Supplier<Optimizable<Long>> optimizableFind = () -> new Optimizable<>() {
+            private long optimum = Long.MAX_VALUE;
+            private long optimumDistance = Long.MAX_VALUE;
+
+            @Override
+            public boolean process(Long value) {
+                if(value == null) return false;
+                long searchValue = 1234L;
+                long distance = Math.abs(value - searchValue);
+                if (distance < optimumDistance) {
+                    optimum = value;
+                    optimumDistance = distance;
+                }
+                return distance == 0;
+            }
+
+            @Override
+            public Long getOptimum() {
+                return optimum;
+            }
+        };
+        binaryValueTree.find(0L, 12, optimizableFind.get());
+    }
     @Test
     public void withRootTest() {
         var tree = Trees.binaryCounter.get();
@@ -45,6 +79,88 @@ public class InfiniteTreeTest {
             }
         };
         assertEquals(expected, tree.find(from, depth, optimizable), message);
+    }
+
+    public static class ModifiedIterator implements Iterator<Long> {
+        private final Iterator<Long> original;
+        private final AtomicInteger count;
+        public ModifiedIterator(AtomicInteger count, Iterator<Long> original) {
+            this.original = original;
+            this.count = count;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return this.original.hasNext();
+        }
+
+        @Override
+        public Long next() {
+            count.getAndIncrement();
+            return this.original.next();
+        }
+    }
+
+    @Test
+    @DisplayName("Should stop immediately if optimizable.process returns true")
+    public void shouldStop(){
+        AtomicInteger count = new AtomicInteger();
+        Function<Long, Iterator<Long>> children = i -> {
+            List<Long> list = new ArrayList<>();
+            if (i == null) {
+                list.add(0L);
+                list.add(1L);
+            } else {
+                list.add(i * 2);
+                list.add(i * 2 + 1);
+            }
+            return new ModifiedIterator(count, list.iterator());
+        };
+        InfiniteTree<Long> binaryValueTree = new InfiniteTree<>(children);
+        binaryTreeHelper(binaryValueTree);
+        assertEquals(2475, count.get());
+    }
+
+    @Test
+    @DisplayName("Should work with 2 consecutive calls")
+    public void shouldWorkWith2Calls(){
+        AtomicInteger count = new AtomicInteger();
+        Function<Long, Iterator<Long>> children = i -> {
+            List<Long> list = new ArrayList<>();
+            if (i == null) {
+                list.add(0L);
+                list.add(1L);
+            } else {
+                list.add(i * 2);
+                list.add(i * 2 + 1);
+            }
+            return new ModifiedIterator(count, list.iterator());
+        };
+        InfiniteTree<Long> binaryValueTree = new InfiniteTree<>(children);
+        binaryTreeHelper(binaryValueTree);
+        binaryTreeHelper(binaryValueTree);
+        assertEquals(4950, count.get());
+    }
+
+    @Test
+    @DisplayName("Should not stop if value is null")
+    public void shouldWorkWithNullValues(){
+        AtomicInteger count = new AtomicInteger();
+        Function<Long, Iterator<Long>> children = i ->{
+            List<Long> list = new ArrayList<>();
+            if (i == null) {
+                list.add(0L);
+                list.add(1L);
+            } else {
+                list.add(null);
+                list.add(null);
+            }
+            return new ModifiedIterator(count, list.iterator());
+        };
+        InfiniteTree<Long> binaryValueTree = new InfiniteTree<>(children);
+        binaryTreeHelper(binaryValueTree);
+        binaryTreeHelper(binaryValueTree);
+        assertEquals(16380, count.get());
     }
 
     private static Stream<Arguments> findTest() {
@@ -102,18 +218,18 @@ public class InfiniteTreeTest {
 
         assertTrue(root.getChildren().get(0).isFullyCalculated());
         assertFalse(root.getChildren().get(1).isFullyCalculated());
-        
+
         // Nach dem Aufruf root.getChildren().get(1).calculateAllChildren():
-        
+
         root.getChildren().get(1).calculateAllChildren();
 
         assertEquals(2, root.getChildren().get(1).getChildren().get(0).getValue());
         assertEquals(3, root.getChildren().get(1).getChildren().get(1).getValue());
 
         assertTrue(root.getChildren().get(1).isFullyCalculated());
-        
+
         // Nach dem Aufruf root.getChildren().get(1).resetChildren():
-        
+
         root.getChildren().get(1).resetChildren();
 
         assertFalse(root.getChildren().get(1).isFullyCalculated());
