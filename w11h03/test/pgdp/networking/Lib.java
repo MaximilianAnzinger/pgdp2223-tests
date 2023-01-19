8748901970a0185bf770eca4986b1439e85a505a
Anatoly Weinstein <44678124+Anatoly03@users.noreply.github.com>
2023-01-19 18:33:24 +0100

    copy pasted into wrong file 💀

--- w11h03/test/pgdp/networking/Lib.java
+++ w11h03/test/pgdp/networking/Lib.java
@@ -1,237 +1,56 @@
 package pgdp.networking;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.fail;
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse.BodyHandlers;
 
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.stream.Collectors;
-
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
-
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-public class NetworkTest {
-
-    private static boolean executeTests = false;
-    private static DataHandler dataHandler = new DataHandler();
-
-    private static String kennung;
-    private static String username;
-    private static String password;
-    private static int id;
-
-    // Don't send NSFW kthx
-
-    private static final int ANATOLYS_ID = 361563966;
-
+public class Lib {
     //
-    // Register the first possible not reserved kennung.
+    // https://github.com/MaximilianAnzinger/pgdp2223-tests/blob/main/w10h02/test/pgdp/teams/Lib.java
     //
 
-    @BeforeAll
-    public static void init() throws Exception {
-        var config = Files
-                .lines(Path.of("test/config.env"))
-                .filter(i -> i.trim().length() != 0)
-                .filter(i -> !i.startsWith("#"))
-                .collect(Collectors.toMap(i -> i.split("=")[0].trim(), i -> i.split("=")[1].trim()));
-
-        kennung = config.get("kennung");
-        username = config.get("username");
-        password = config.get("password");
-
-        if (config.get("kennung").equals("null")) {
-            System.err.println("""
-                    [STEP 1] To setup the Tests, please provide your
-                    TUM kennung in the `config.env` file and run the
-                    tests again. This execution all tests will fail.
-                    """);
-            return;
-        }
-
-        if (config.get("username").equals("null")) {
-            System.err.println("""
-                    [STEP 2] Please provide a username to your
-                    config file.
-                    """);
-            return;
-        }
-
-        if (config.get("password").equals("null")) {
-            assertTrue(dataHandler.register(username, kennung));
-
-            System.err.println("""
-                    [STEP 3] An email was sent to your mailbox
-                    associated with the TUM kennung. Please open
-                    it and provide the config file with the pass-
-                    word you have received.
-                    """);
-            return;
-        }
-
-        Lib.getField(dataHandler, "username").set(dataHandler, username);
-        Lib.getField(dataHandler, "password").set(dataHandler, password);
-
-        executeTests = true;
-        System.out.println("RUNNING TESTS...");
+    public static <T, F> Field getField(T obj, String fieldName)
+            throws NoSuchFieldException, IllegalAccessException {
+        Field field = obj.getClass().getDeclaredField(fieldName);
+        field.setAccessible(true);
+        return field;
     }
 
-    @BeforeEach
-    public void configError() {
-        if (!executeTests)
-            fail("Please follow the instructions to set up your test files.");
+    public static <T, F> Method getMethod(T obj, String fieldName)
+            throws NoSuchMethodException, IllegalAccessException {
+        Method method = obj.getClass().getDeclaredMethod(fieldName);
+        method.setAccessible(true);
+        return method;
     }
 
     //
-    // BEHAVIOUR TEST
+    // Set user private/ public
     //
 
-    //
-    // FLAGS
-    //
-    // [A] - Test that should <A>lawys pass and not be affected by code.
-    // [P] - Test in <P>ublic environment.
-    //
+    public static boolean makePublic(DataHandler dataHandler, boolean value)
+            throws NoSuchFieldException, IllegalAccessException {
+        var serverAddress = (String) getField(dataHandler, "serverAddress").get(dataHandler);
+        var client = (HttpClient) getField(dataHandler, "client").get(dataHandler);
 
-    @Test
-    @Order(1)
-    @DisplayName("should request token")
-    public void requestTokenTest() {
-        assertNotNull(dataHandler.requestToken());
-    }
-
-    @Test
-    @Order(2)
-    @DisplayName("should set visibility to private")
-    public void setVisibilityToPrivate() throws Exception {
-        assertTrue(Lib.makePublic(dataHandler, false));
-    }
-
-    @Test
-    @Order(3)
-    @DisplayName("should login with credentials")
-    public void loginTest() throws Exception {
-        assertTrue(dataHandler.login(username, password));
-
-        id = (int) Lib.getField(dataHandler, "id").get(dataHandler);
-        System.out.println(id);
-
-        assertNotNull(id);
-        assertTrue(id > 0);
-    }
-
-    @Test
-    @Order(4)
-    @DisplayName("should get contacts")
-    public void getContactsTest() throws Exception {
-        var users = dataHandler.getContacts();
-
-        assertTrue(users.containsKey(id));
-        assertEquals(username, users.get(id).name());
-    }
-
-    @Test
-    @Order(5)
-    @DisplayName("should get messages with user zero")
-    public void getMessagesWithUserTest() throws Exception {
-        assertNotNull(dataHandler.getMessagesWithUser(0, 0, 0));
-    }
-
-    @Test
-    @Order(6)
-    @DisplayName("should connect")
-    public void connectTest() throws Exception {
         //
-        // TODO check if `Input Handler started` was printed into console?
+        // TEMPLATE CODE
         //
-        Lib.getMethod(dataHandler, "connect").invoke(dataHandler);
 
-        assertTrue(dataHandler.connected);
-        assertNotNull(Lib.getField(dataHandler, "socket").get(dataHandler));
-        assertNotNull(Lib.getField(dataHandler, "in").get(dataHandler));
-        assertNotNull(Lib.getField(dataHandler, "out").get(dataHandler));
-    }
+        HttpRequest request = HttpRequest
+                .newBuilder(URI.create("http://" + serverAddress + "/api/user/me/setpublic/" + value))
+                .header("Authorization", "Bearer " + dataHandler.requestToken())
+                .GET()
+                .build();
 
-    @Test
-    @Order(7)
-    @DisplayName("[A] should switch partner")
-    public void partnerSwitchTest() throws Exception {
-        // If not crash -> Test went good?
-        dataHandler.switchConnection(id);
-    }
-
-    @Test
-    @Order(8)
-    @DisplayName("[A] should send message")
-    public void sendMessageTest() throws Exception {
-        // If not crash -> Test went good?
-        dataHandler.sendMessage("Hello, Pingu!");
-    }
-
-    @Test
-    @Order(9)
-    @DisplayName("should check `sendMessageTest` by actually checking if messages were updated")
-    public void updatedMessagesTest() throws Exception {
-        var _todo = dataHandler.getMessagesWithUser(0, 0, 0);
-        System.out.println(_todo);
-    }
-
-    @Test
-    @Order(10)
-    @DisplayName("[A] should set visibility to public")
-    public void setVisibilityToPublic() throws Exception {
-        assertTrue(Lib.makePublic(dataHandler, true));
-    }
-
-    @Test
-    @Order(11)
-    @DisplayName("[P] should get contacts")
-    public void getContactsPublicTest() throws Exception {
-        // TODO
-        var users = dataHandler.getContacts();
-
-        assertTrue(users.containsKey(id));
-        assertEquals(username, users.get(id).name());
-    }
-
-    @Test
-    @Order(12)
-    @DisplayName("[P] should get messages with user zero")
-    public void getMessagesWithUserPublicTest() throws Exception {
-        // TODO
-        assertNotNull(dataHandler.getMessagesWithUser(0, 0, 0));
-    }
-
-    @Test
-    @Order(13)
-    @DisplayName("[P] [A] should switch partner to anatoly")
-    public void partnerSwitchPublicTest() throws Exception {
-        // TODO
-        dataHandler.switchConnection(ANATOLYS_ID);
-    }
-
-    @Test
-    @Order(14)
-    @DisplayName("[P] [A] should send message")
-    public void sendMessagePublicTest() throws Exception {
-        // TODO
-        dataHandler.sendMessage("Hello, Pingu!");
-    }
-
-    @Test
-    @Order(15)
-    @DisplayName("[P] should check `sendMessageTest` by actually checking if messages were updated")
-    public void updatedMessagesPublicTest() throws Exception {
-        // TODO
-        var _todo = dataHandler.getMessagesWithUser(0, 0, 0);
-        System.out.println(_todo);
+        try {
+            client.send(request, BodyHandlers.ofString());
+            return true;
+        } catch (IOException | InterruptedException e) {
+            return false;
+        }
     }
 }
