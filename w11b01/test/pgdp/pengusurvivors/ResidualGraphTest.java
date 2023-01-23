3030fe2c6c3a72952b61658c6fa0a6b7f3ca9da2
Julian Hartl <julian.hartl.dev@gmail.com>
2023-01-23 18:20:14 +0100

    remove persistent connection of graph

--- w11b01/test/pgdp/pengusurvivors/ResidualGraphTest.java
+++ w11b01/test/pgdp/pengusurvivors/ResidualGraphTest.java
@@ -107,8 +107,8 @@ public class ResidualGraphTest {
                 ),
                 Arguments.of(
                         """
-                                a -> b (1/1);
-                                b -> (0/2);
+                                a -> b (0/1);
+                                b -> (0/0);
                                 """,
                         "a", "c", List.of()
                 ),
