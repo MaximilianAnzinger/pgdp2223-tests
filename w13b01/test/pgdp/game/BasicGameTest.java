0a9090e3a442de84689506137ee3a0b2123ac749
Cedric Tölg <cedric.toelg@tum.de>
2023-02-09 02:08:41 +0100

    [w13b01] added basic Human Simulator

--- w13b01/test/pgdp/game/BasicGameTest.java
+++ w13b01/test/pgdp/game/BasicGameTest.java
@@ -3,10 +3,12 @@ package pgdp.game;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
 
 import java.io.*;
 import java.nio.file.Files;
 
+//THESE TESTS PRODUCE OUTPUT FILES. IN CASE OF WRONG IMPLEMENTATION, THE HUMAN GAME MIGHT PRODUCE A RELATIVELY LARGE OUTPUT FILE (<1 MB)
 public class BasicGameTest
 {
     private PinguGame pinguGame;
@@ -67,6 +69,25 @@ public class BasicGameTest
         String expectedString = Files.readString(expected.toPath());
         String actualString = Files.readString(actual.toPath());
         Assertions.assertEquals(expectedString, actualString);
+    }
 
+    //SIMULATES ONE HUMAN PLAYER, MIGHT TIME OUT IF WRONG OR SLOW IMPLEMENTATION AND PRODUCE MODERATELY LARGE OUTPUT FILE
+    @Test
+    @Timeout(value = 1, threadMode = Timeout.ThreadMode.SEPARATE_THREAD)
+    @DisplayName("One Human Player, standard Seed, if it timeouts, its wrong")
+    public void HumanPlayerStandardSeed() throws IOException
+    {
+        File expected = new File("./test/pgdp/game/HumanPlayerStandardSeed.txt");
+        File actual = new File("./test/pgdp/game/Out_HumanPlayerStandardSeed.txt");
+
+        PrintStream t = new PrintStream(actual);
+        System.setOut(t);
+        ByteArrayInputStream in = new ByteArrayInputStream("1\n1\n2\n1\n1\n1\n2\n1\n2\n1\n1\n1\n2\n1\n1\n1\n2\n1\n2\n1\n1\n1\n2\n1\n1\n1\n2\n1\n2\n1\n2\n1\n1\n1\n2\n1\n2\n1\n2\n1\n1\n1\n3\n3\n3\n3\n3\n3\n3\n3\n3\n3\n3\n3\n0\n".getBytes());
+        System.setIn(in);
+        PinguGame c = new PinguGame(true, 420, 5);
+        c.play();
+        String expectedString = Files.readString(expected.toPath());
+        String actualString = Files.readString(actual.toPath());
+        Assertions.assertEquals(expectedString, actualString);
     }
 }
